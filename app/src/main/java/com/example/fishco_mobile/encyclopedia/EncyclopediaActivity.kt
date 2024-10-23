363cb0f3efd587405bfe446f99843abfddfbc37f
mirfan777 <fcmirfan122@gmail.com>
2024-10-23 21:35:54 +0700

    adding fonts and folder structure

---
+++ app/src/main/java/com/example/fishco_mobile/encyclopedia/EncyclopediaActivity.kt
@@ -0,0 +1,2 @@
+package com.example.fishco_mobile.encyclopedia
+
--- app/src/main/java/com/example/fishco_mobile/ui/theme/Color.kt
+++ app/src/main/java/com/example/fishco_mobile/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.example.fishco_mobile.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Primary Colors
+val Primary50 = Color(0xFFF0F9FF)
+val Primary100 = Color(0xFFE0F2FE)
+val Primary200 = Color(0xFFBAE2FD)
+val Primary300 = Color(0xFF7DC9FC)
+val Primary400 = Color(0xFF38ABF8)
+val Primary500 = Color(0xFF0E91E9)
+val Primary600 = Color(0xFF0278C7)
+val Primary700 = Color(0xFF0362A1)
+val Primary800 = Color(0xFF075385)
+val Primary900 = Color(0xFF0C476E)
+val Primary950 = Color(0xFF082F49)
